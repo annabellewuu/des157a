be969dfe77da2f7babe629a79b1204f21dc8d0d7
Annabelle Wu <annabellewu@gmail.com>
2023-10-25 00:33:18 -0700

    madlib progress

--- studio1/script.js
+++ studio1/script.js
@@ -1,5 +1,64 @@
 // script.js
-document.getElementById('generate').addEventListener('click', function () {
+(function(){
+    'use strict';
+    console.log('reading js');
+
+    const myForm = document.querySelector('#myform');
+    const madLib = document.querySelector("#madlib");
+
+
+    myForm.addEventListener('submit', function(event){
+        event.preventDefault();
+        const adj1 = document.querySelector('#adj1').value;
+        const name1 = document.querySelector('#name1').value;
+        const color = document.querySelector('#color').value;
+        const noun1 = document.querySelector('#noun1').value;
+        const object = document.querySelector('#object').value;
+        const bodypart = document.querySelector('#bodypart').value;
+        const adj2 = document.querySelector('#adj2').value;
+        const sound = document.querySelector('#sound').value;
+
+
+        /* const myText = `<p>You typed the words: ${noun1}, ${noun2}, ${adj}, ${verb}</p>`; */
+
+        let myText = '';
+
+        if(adj1 == ''){
+            myText = "Please give me an adjective:"
+            document.querySelector('#adj1').focus();
+        } 
+        else if(name1 == ''){
+            myText = "Please give me a name!"
+            document.querySelector('#name1').focus();
+        }
+        else if(adj == ''){
+            myText = "Please provide an adjective!"
+            document.querySelector('#adj').focus();
+        
+        else if(verb == ''){
+            myText = "Please provide a verb!"
+            document.querySelector('#verb1').focus();
+        }
+        else{
+            myText = `<p>You typed the words: ${noun1}, ${noun2}, ${adj}, ${verb}</p>`;
+            document.querySelector('#noun1').value = '';
+            document.querySelector('#noun2').value = '';
+            document.querySelector('#adj').value = '';
+            document.querySelector('#verb1').value = '';
+        }
+
+        madLib.innerHTML = myText;
+    
+        
+    });
+
+})();
+
+
+
+
+
+/* document.getElementById('generate').addEventListener('click', function () {
     const noun = document.getElementById('noun').value;
     const verb = document.getElementById('verb').value;
     const adjective = document.getElementById('adjective').value;
@@ -7,4 +66,4 @@ document.getElementById('generate').addEventListener('click', function () {
     const story = `Once upon a time, there was a ${adjective} ${noun}. It loved to ${verb} all day long. The end.`;
 
     document.getElementById('story').textContent = story;
-});
+}); */
