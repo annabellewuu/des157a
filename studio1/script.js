1afd07b9c25a68d95909dfd0a416454f8bc4af51
Annabelle Wu <annabellewu@gmail.com>
2023-10-30 13:32:58 -0700

    mad libs responsive

--- studio1/script.js
+++ studio1/script.js
@@ -31,24 +31,6 @@
         });
     });
 
-    document.querySelector('.open').addEventListener('click', function(event){
-        event.preventDefault();
-        document.getElementById('overlay').className = 'hidden';
-
-    });
-
-    document.querySelector('.close').addEventListener('click', function(event){
-        event.preventDefault();
-        document.getElementById('overlay').className = 'showing';
-
-    });
-
-    document.addEventListener('keydown', function (event){
-        if (event.key == "Escape"){
-            document.getElementById('overlay').className = 'hidden';
-        }
-    });
-
 })();
 
 
